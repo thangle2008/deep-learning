9bd4352e73ab1041a41310d20970037f000225c5
bilawalss <bilawal.shaikh@richmond.edu>
2017-07-14 13:18:37 -0400

    add script file for downloading bird images

---
+++ data/get_bird.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget 'https://drive.google.com/uc?export=download&id=0B1jo1AcPnGtFSnU5cUVEUi1HalU' -O bird_342x256.zip
+mkdir -p images
+unzip bird_342x256.zip -d ./images
+rm -rf bird_342x256.zip
